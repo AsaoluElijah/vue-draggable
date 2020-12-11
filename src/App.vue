f8ec6c224ad51c3a941b1b2871f24ef2fe8dadae
Asaolu Elijah <asaoluelijah7@gmail.com>
2020-12-11 12:31:03 +0100

    added pie chart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <rough-bar
-      :data="{
-        labels: ['North', 'South', 'East', 'West'],
-        values: [10, 5, 8, 3],
-      }"
-      title="Regions"
-      roughness="8"
-      :colors="['red', 'orange', 'blue', 'skyblue']"
-      stroke="black"
-      stroke-width="3"
-      fill-style="cross-hatch"
-      fill-weight="3.5"
-      class="d-inline"
-    />
+    <div class="d-inline-block">
+      <rough-bar
+        :data="{
+          labels: ['North', 'South', 'East', 'West'],
+          values: [10, 5, 8, 3],
+        }"
+        title="Regions"
+        roughness="8"
+        :colors="['red', 'orange', 'blue', 'skyblue']"
+        stroke="black"
+        stroke-width="3"
+        fill-style="cross-hatch"
+        fill-weight="3.5"
+      />
+    </div>
+    <div class="d-inline-block">
+      <rough-pie
+        :data="{
+          labels: ['North', 'South', 'East', 'West'],
+          values: [10, 5, 8, 3],
+        }"
+        title="Covid-19 Chart"
+        roughness="8"
+        :colors="['red', 'orange', 'blue', 'skyblue']"
+        stroke="black"
+        stroke-width="3"
+        fill-style="cross-hatch"
+        fill-weight="3.5"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { RoughBar } from "vue-roughviz";
+import { RoughPie } from "vue-roughviz";
 export default {
   name: "App",
   components: {
     RoughBar,
+    RoughPie,
   },
 };
-</script> 
+</script>
+<style scoped>
+#app {
+  text-align: center;
+  margin-top: 10vh;
+}
+.d-inline-block {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
